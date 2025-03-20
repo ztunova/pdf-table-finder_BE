5c9cda233b5e2ae03f52c7dbfef6a19db18ac381
zofka <zofka.tunova@gmail.com>
2025-03-20 16:51:49 +0100

    add param type

--- src/custom_types/interfaces.py
+++ src/custom_types/interfaces.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import Dict, List
+
+from src.custom_types.api_types import Point, SingleTableRequest
 
 
 class TableDetectionInterface(ABC):
     @abstractmethod
-    def detect_tables(self):
+    def detect_tables(self) -> Dict[int, List[Point]]:
         pass
 
 
 class TableExtractionInterface(ABC):
     @abstractmethod
-    def extract_tabular_data(self):
+    def extract_tabular_data(self, rectangle_data: SingleTableRequest):
         pass
